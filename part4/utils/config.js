4d8bb56a2317eed232c66832743b85ddc70fe4c4
ryaplots <56658938+ryaplots@users.noreply.github.com>
2021-03-09 00:02:42 +0000

    part4

---
+++ part4/utils/config.js
@@ -0,0 +1,7 @@
+const PORT = 3003
+const MONGODB_URI = 'mongodb+srv://darya98:KMxOnLVuGytyR4ID@cluster0.89x5h.mongodb.net/Blog?retryWrites=true&w=majority'
+
+module.exports = {
+    MONGODB_URI,
+    PORT
+}
