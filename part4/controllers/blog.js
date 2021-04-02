d0b5bd02a7e0e51810dff64a93480c48a775c40c
ryaplots <56658938+ryaplots@users.noreply.github.com>
2021-04-02 16:39:51 +0100

    tests and user administration

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,17 +1,45 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
 blogRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
+
     if (!request.body.title || !request.body.url) {
         return response.status(400).end()
     }
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(body.userId)
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
     const savedBlog = await blog.save()
+    response.status(201).json(savedBlog.toJSON())
+    await user.save()
     response.status(201).json(savedBlog.toJSON())
 })
 
@@ -25,7 +53,7 @@ blogsRouter.put('/:id', async (request, response) => {
     const blog = {
         likes: body.likes
     }
-  
+
     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
     response.status(200).json(updatedBlog.toJSON())
 })
--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -2,21 +2,28 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+    const users = await User
+        .find({}).populate('blogs', { url: 1, title: 1, author: 1 })
+    response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
-  const body = request.body
+    const body = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+        blogs: body.blogs
+    })
 
-  const savedUser = await user.save()
+    const savedUser = await user.save()
 
-  response.json(savedUser)
+    response.status(201).json(savedUser.toJSON())
 })
 
 module.exports = usersRouter
